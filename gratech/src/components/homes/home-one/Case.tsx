ed9c2b38dbe837dca07807ab50ad2dfe27c02bfb
Kajal Rathod <kajal.rathod@ifour.com>
2025-07-31 11:44:13 +0530

    add Why Choose Us as Your Software Development Agency section

--- gratech/src/components/homes/home-one/Case.tsx
+++ gratech/src/components/homes/home-one/Case.tsx
@@ -11,6 +11,10 @@ import about_thumb2 from "@/assets/images/about/about-two-image2.png"
 import shape_2 from "@/assets/images/shape/about-two-dot.png"
 import shape_3 from "@/assets/images/shape/about-circle-helper.png"
 import shape_4 from "@/assets/images/shape/about-two-circle.png"
+import about_thumb3 from "@/assets/images/about/about-image1.jpg"
+import about_thumb4 from "@/assets/images/about/about-image2.png"
+import shape_5 from "@/assets/images/shape/about-line.png"
+import shape_6 from "@/assets/images/shape/about-circle.png"
 
 const setting = {
    slidesPerView: 4,
@@ -107,6 +111,87 @@ const Case = () => {
                         <Link href="/about" className="btn-one">
                            Let's Get Started <i className="fa-regular fa-arrow-right-long"></i>
                         </Link>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+
+         {/* A Software Development Company You Can Trust Section */}
+         <section id="about-section" className="about-area sub-bg pt-120 pb-120">
+            <div className="about__shape wow slideInLeft" data-wow-delay="400ms" data-wow-duration="1500ms">
+               <Image src={shape_5} alt="shape" />
+            </div>
+            <div className="container">
+               <div className="row g-4">
+                  <div className="col-xl-6 wow fadeInLeft" data-wow-delay="200ms" data-wow-duration="1500ms">
+                     <div className="section-header mb-40">
+                        <h5 className="wow fadeInUp" data-wow-delay="00ms" data-wow-duration="1500ms">
+                           <Image className="me-1" src={shape_1} alt="icon" />
+                           ABOUT iFour Technolab
+                        </h5>
+                        <h2 className="wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1500ms">
+                           Why Choose Us as Your Software Development Agency?
+                        </h2>
+                        <p className="wow fadeInUp" data-wow-delay="400ms" data-wow-duration="1500ms">
+                           At iFour Technolab, what differentiates us from others is our steadfast dedication to innovation and making sustainable success a goal that our clients can easily achieve. With our focus set on this objective, we deliver value-driven solutions and services to clients that act as catalysts for long-term prosperity. Our highlights include:
+                        </p>
+
+                        <div className="row g-4 mt-4">
+                           <div className="col-12 wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1500ms">
+                              <div className="feature-item">
+                                 <div className="d-flex align-items-start">
+                                    <div className="icon me-4">
+                                       <i className="fa-solid fa-users-gear" style={{ fontSize: '48px', color: '#0f7a95' }}></i>
+                                    </div>
+                                    <div className="content">
+                                       <h4 className="mb-3">Expert Team</h4>
+                                       <p>We have assembled a team of 120+ veteran software developers capable of building top-grade bespoke software solutions based on your specifications.</p>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                           <div className="col-12 wow fadeInUp" data-wow-delay="400ms" data-wow-duration="1500ms">
+                              <div className="feature-item">
+                                 <div className="d-flex align-items-start">
+                                    <div className="icon me-4">
+                                       <i className="fa-solid fa-headset" style={{ fontSize: '48px', color: '#0f7a95' }}></i>
+                                    </div>
+                                    <div className="content">
+                                       <h4 className="mb-3">Round-the-Clock Support</h4>
+                                       <p>We have a diligent tech support team that is always available for hands-on support, responds to client questions and queries, and addresses them.</p>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                           <div className="col-12 wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
+                              <div className="feature-item">
+                                 <div className="d-flex align-items-start">
+                                    <div className="icon me-4">
+                                       <i className="fa-solid fa-microchip" style={{ fontSize: '48px', color: '#0f7a95' }}></i>
+                                    </div>
+                                    <div className="content">
+                                       <h4 className="mb-3">Modern Tech Expertise</h4>
+                                       <p>We remain on the cutting edge of technology, adopting new trends and advancements as they unfold. This up-to-date knowledge and our extensive industry expertise enable us to build unique solutions.</p>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+
+                     </div>
+                  </div>
+                  <div className="col-xl-6 wow fadeInRight" data-wow-delay="200ms" data-wow-duration="1500ms">
+                     <div className="about__left-item">
+                        <div className="image big-image">
+                           <Image src={about_thumb3} alt="image" />
+                        </div>
+                        <div className="image sm-image">
+                           <Image src={about_thumb4} alt="image" />
+                        </div>
+                        <div className="circle-shape">
+                           <Image src={shape_6} alt="shape" />
+                        </div>
                      </div>
                   </div>
                </div>
