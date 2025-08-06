bebdc0770d2fcdfdc860e877871ffd62cc3daad3
Kajal Rathod <kajal.rathod@ifour.com>
2025-08-06 19:06:03 +0530

    service6 development complete

--- gratech/src/app/blockchain-development-company/page.tsx
+++ gratech/src/app/blockchain-development-company/page.tsx
@@ -1,4 +1,4 @@
-import ServiceDetails from "@/components/services/service-details";
+import ServiceDetails from "@/components/services/service-details6";
 import Wrapper from "@/layouts/Wrapper";
 
 export const metadata = {
