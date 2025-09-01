5a2ad8910171f002c6e946b84d40adf497ee5167
Kajal Rathod <kajal.rathod@ifour.com>
2025-09-01 14:18:18 +0530

    fixed counter banner issue:

--- gratech/src/components/common/BreadCrumb.tsx
+++ gratech/src/components/common/BreadCrumb.tsx
@@ -154,18 +154,29 @@ const BreadCrumb = ({ sub_title, title }: DataType) => {
 
    const hireContent = gethireContent(title);
 
-   return (
-      <section className="banner__inner-page bg-image pt-180 pb-180 bg-image"
-         style={{ backgroundImage: `url(/assets/images/banner/banner-inner-page.jpg)` }}>
-         <div className="shape2 wow slideInLeft" data-wow-delay="00ms" data-wow-duration="1500ms">
-            <Image src={shape_1} alt="shape" />
-         </div>
-         <div className="shape1 wow slideInLeft" data-wow-delay="200ms" data-wow-duration="1500ms">
-            <Image src={shape_2} alt="shape" />
-         </div>
-         <div className="shape3 wow slideInRight" data-wow-delay="200ms" data-wow-duration="1500ms">
-            <Image className="sway__animationX" src={shape_3} alt="shape" />
-         </div>
+       return (
+       <section className="banner__inner-page bg-image pt-180 pb-180 bg-image"
+          style={{ 
+             backgroundImage: `linear-gradient(135deg, rgba(15, 122, 149, 0.3) 0%, rgba(13, 107, 127, 0.3) 50%, rgba(10, 90, 106, 0.3) 100%), url(/assets/images/banner/banner-inner-page.jpg)`,
+             backgroundSize: 'cover',
+             backgroundPosition: 'center',
+             backgroundRepeat: 'no-repeat'
+          }}>
+          {/* Left side shapes - only on home page */}
+          {title === "Home" && (
+             <>
+                <div className="shape2 wow slideInLeft" data-wow-delay="00ms" data-wow-duration="1500ms">
+                   <Image src={shape_1} alt="shape" />
+                </div>
+                <div className="shape1 wow slideInLeft" data-wow-delay="200ms" data-wow-duration="1500ms">
+                   <Image src={shape_2} alt="shape" />
+                </div>
+             </>
+          )}
+          {/* Right side shape - always show */}
+          <div className="shape3 wow slideInRight" data-wow-delay="200ms" data-wow-duration="1500ms">
+             <Image className="sway__animationX" src={shape_3} alt="shape" />
+          </div>
          <div className="container">
             <h2 className="wow fadeInUp" data-wow-delay="00ms" data-wow-duration="1500ms">{title}</h2>
             <div className="breadcrumb-list wow fadeInUp" data-wow-delay="200ms" data-wow-duration="1500ms">
